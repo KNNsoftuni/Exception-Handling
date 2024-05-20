449709fe178589ea8c8ce1c18046bf39471efb90
KNNsoftuni <Konstantin.nikolov.tino@gmail.com>
2024-05-20 23:53:04 +0300

    Exception Handling Lab
    Exception Handling Lab - Incomplete

---
+++ ExceptionHandlingLab/Square Root/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (input < 0)
+                {
+                    throw new Exception($"Invalid Number");
+                }
+
+                Console.WriteLine(Math.Sqrt(input));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+        }
+    }
+}
